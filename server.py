420b2410bcac5da38a6a1bacfba515e0809813cf
asya-code <77514066+asya-code@users.noreply.github.com>
2022-06-29 23:08:35 -0700

    templates updated, model added, crud updated,satasourse added

---
+++ server.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request, flash, session, redirect, jsonify
+#from model import connect_to_db, db, User, Reservation
+import os
+import crud
+from jinja2 import StrictUndefined
+#from dateutil.parser import parse
+from random import choice, randint
+
+
+app = Flask(__name__)
+app.sekret_key="dev"
+app.jinja_env.undefined = StrictUndefined
+
+@app.route("/about")
+def main_page():
+    return render_template("about.html")
+
+@app.route("/")
+def homepage():
+    return render_template("homepage.html")
+
+@app.route("/passage")
+def passage():
+    return passage
+
+@app.route("/random")
+def random_quote():
+    quote = crud.get_random_quote()
+    return render_template("passage.html", quote=quote)
+    
+@app.route("/search")
+def search():
+    author = request.args.get('author').lower()
+    quote = crud.get_quote_by_author(author)
+    return render_template("passage.html", quote=quote)
+
+if __name__ == "__main__":
+    #connect_to_db(app)
+    app.run(host="0.0.0.0", debug=True)
